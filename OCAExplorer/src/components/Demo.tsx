d46208a3b824ebce60a29d4d90cfa319aadb96a5
Gavin <gavinfreeborn@gmail.com>
2023-05-18 16:25:55 -0700

    Made use of the title class attribute
    Signed-off-by: Gavin <gavinfreeborn@gmail.com>

--- OCAExplorer/src/components/Demo.tsx
+++ OCAExplorer/src/components/Demo.tsx
@@ -8,100 +8,83 @@ const demoStates = {
       target: "body",
       disableBeacon: true,
       position: "center",
+      title: <Typography variant="h3">What Is OCA Explorer?</Typography>,
       content: (
-	<div>
-	  <Typography variant="h3">What Is OCA Explorer?</Typography>
-	  <Typography variant="h5">
-	    The OCA Explorer is intended to assist in the creating of creating&nbsp;
-	    <Link href="https://oca.colossi.network/">Overlays Capture Architecture(OCA)</Link>
-	&nbsp;
+	<Typography variant="h5">
+	  The OCA Explorer is intended to assist in the creating of creating&nbsp;
+	  <Link href="https://oca.colossi.network/">Overlays Capture Architecture(OCA)</Link>
+	         &nbsp;
 	bundles and previewing
-	&nbsp;
+	         &nbsp;
 	<Link href="https://github.com/swcurran/aries-rfcs/tree/oca4aries/features/0755-oca-for-aries#aries-specific-branding-overlay">Branding Overlays</Link>.
 	  </Typography>
-	</div>
       )
     },
     {
       target: '#overlay-bundle-id',
       position: "center",
       disableBeacon: true,
+      title: <Typography variant="h3">Selecting an OCA Bundle</Typography>,
       content: (
-	<div>
-	  <Typography variant="h3">Selecting an OCA Bundle</Typography>
-	  <Typography variant="h5">
-	    Here you can select a Pre-existing OCA Bundle
-	  </Typography>
-	</div>
+	<Typography variant="h5">
+	  Here you can select a Pre-existing OCA Bundle
+	</Typography>
       )
     },
     {
       target: '#upload-oca-bundle-button',
+      title: <Typography variant="h3">Uploading a new OCA Bundle</Typography>,
       content: (
-	<div>
-	  <Typography variant="h3">Uploading a new OCA Bundle</Typography>
-	  <Typography variant="h5">
-	    If you would like to upload your own OCA Bundle click here
-	  </Typography>
-	</div>
+	<Typography variant="h5">
+	  If you would like to upload your own OCA Bundle click here
+	</Typography>
       ),
     },
   ],
   brandingSteps : [
     {
       target: '#overlay-bundle-language-select',
-      disableBeacon: true,
+      title: <Typography variant="h3">Previewing Languages</Typography>,
       content: (
-	<div>
-	  <Typography variant="h3">Previewing Languages</Typography>
-	  <Typography variant="h5">
-	    To preview alternative languages select one of the following radio buttons
-	  </Typography>
-	</div>
+	<Typography variant="h5">
+	  To preview alternative languages select one of the following radio buttons
+	</Typography>
       ),
     },
     {
       target: '#overlay-bundle-credential-card',
+      title: <Typography variant="h3">Credential List Layout</Typography>,
       content: (
-	<div>
-	  <Typography variant="h3">Credential List Layout</Typography>
-	  <Typography variant="h5">
-	    This is the current Credential List Layout
-	  </Typography>
-	</div>
+	<Typography variant="h5">
+	  This is the current Credential List Layout
+	</Typography>
       ),
     },
     {
       target: '#overlay-bundle-credential-details',
+      title: <Typography variant="h3">Single Credential Layout</Typography>,
       content: (
-	<div>
-	  <Typography variant="h3">Single Credential Layout</Typography>
-	  <Typography variant="h5">
-	    Here is the current Single Credential Layout
-	  </Typography>
-	</div>
+	<Typography variant="h5">
+	  Here is the current Single Credential Layout
+	</Typography>
       ),
     },
     {
       target: '#overlay-bundle-branding-form-fields',
+      title: <Typography variant="h3">Customizing Branding</Typography>,
       content: (
-	<div>
-	  <Typography variant="h3">Customizing Branding</Typography>
-	  <Typography variant="h5">
-	    Here you can customize the branding for your overlay
-	  </Typography>
-	</div>
+	<Typography variant="h5">
+	  Here you can customize the branding for your overlay
+	</Typography>
       ),
     },
     {
       target: "#overlay-branding-download-branding-overlay",
+      title: <Typography variant="h3">Download Your Newly Created Branding</Typography>,
       content: (
-	<div>
-	  <Typography variant="h3">Download Your Newly Created Branding</Typography>
-	  <Typography variant="h5">
-	    Here you can customize the branding for your overlay
-	  </Typography>
-	</div>
+	<Typography variant="h5">
+	  Here you can customize the branding for your overlay
+	</Typography>
       ),
   }]
 }
