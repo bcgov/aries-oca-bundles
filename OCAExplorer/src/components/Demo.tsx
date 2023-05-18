777b5f731c2234663972ee2f88337464196041de
Gavin <gavinfreeborn@gmail.com>
2023-05-18 13:42:02 -0700

    correct formatting of demosteps
    Signed-off-by: Gavin <gavinfreeborn@gmail.com>

--- OCAExplorer/src/components/Demo.tsx
+++ OCAExplorer/src/components/Demo.tsx
@@ -4,103 +4,107 @@ import Joyride, {STATUS} from 'react-joyride';
 import { Theme } from '@mui/material/styles';
 
 const demoStates = {
-    introSteps: [
-	{
-            target: "body",
-            disableBeacon: true,
-            position: "center",
-            content: (
-		<div>
-		    <Typography variant="h3">What Is OCA Explorer?</Typography>
-		    <Typography variant="h5">
-		    The OCA Explorer is intended to assist in the creating of creating Overlays Capture Architecture (OCA)bundles and previewing&nbsp;
-		    <Link href="https://github.com/swcurran/aries-rfcs/tree/oca4aries/features/0755-oca-for-aries#aries-specific-branding-overlay">Branding Overlays</Link>.
-		    </Typography>
-		    </div>
-            )
-	},
-	{
-            target: '#overlay-bundle-id-label',
-            position: "center",
-            disableBeacon: true,
-            content: (
-		<div>
-		    <Typography variant="h3">Selecting an OCA Bundle?</Typography>
-		    <Typography variant="h5">
-		    Here you can select a Pre-existing OCA Bundle
-		</Typography>
-		    </div>
-            )
-	},
-	{
-            target: '#upload-oca-bundle-button',
-            content: (
-		<div>
-		    <Typography variant="h3">Uploading a new OCA Bundle?</Typography>
-		    <Typography variant="h5">
-		    If you would like to upload your own OCA Bundle click here
-		</Typography>
-		    </div>
-            ),
-	},
-    ],
-    brandingSteps : [
-	{
-            target: '#overlay-bundle-language-select',
-            disableBeacon: true,
-            content: (
-		<div>
-		    <Typography variant="h3">Previewing Languages</Typography>
-		    <Typography variant="h5">
-		    To preview alternative languages select one of the following radio buttons
-		</Typography>
-		    </div>
-            ),
-	},
-	{
-            target: '#overlay-bundle-credential-card',
-            content: (
-		<div>
-		    <Typography variant="h3">Credential List Layout</Typography>
-		    <Typography variant="h5">
-		    This is the current Credential List Layout
-		</Typography>
-		    </div>
-            ),
-	},
-	{
-            target: '#overlay-bundle-credential-details',
-            content: (
-		<div>
-		    <Typography variant="h3">Single Credential Layout</Typography>
-		    <Typography variant="h5">
-		    Here is the current Single Credential Layout
-		</Typography>
-		    </div>
-            ),
-	},
-	{
-            target: '#overlay-bundle-branding-form',
-            content: (
-		<div>
-		    <Typography variant="h3">Customizing Branding</Typography>
-		    <Typography variant="h5">
-		    Here you can customize the branding for your overlay
-		</Typography>
-		    </div>
-            ),
-	},
-	{
-            target: "#overlay-branding-download-branding-overlay",
-            content: (
-		<div>
-		    <Typography variant="h3">Download Your Newly Created Branding</Typography>
-		    <Typography variant="h5">
-		    Here you can customize the branding for your overlay
-		</Typography>
-		    </div>
-            ),
-	}]
+  introSteps: [
+    {
+      target: "body",
+      disableBeacon: true,
+      position: "center",
+      content: (
+	<div>
+	  <Typography variant="h3">What Is OCA Explorer?</Typography>
+	  <Typography variant="h5">
+	    The OCA Explorer is intended to assist in the creating of creating&nbsp;
+	    <Link href="https://oca.colossi.network/">Overlays Capture Architecture(OCA)</Link>
+	&nbsp;
+	bundles and previewing
+	&nbsp;
+	<Link href="https://github.com/swcurran/aries-rfcs/tree/oca4aries/features/0755-oca-for-aries#aries-specific-branding-overlay">Branding Overlays</Link>.
+	  </Typography>
+	</div>
+      )
+    },
+    {
+      target: '#overlay-bundle-id-label',
+      position: "center",
+      disableBeacon: true,
+      content: (
+	<div>
+	  <Typography variant="h3">Selecting an OCA Bundle?</Typography>
+	  <Typography variant="h5">
+	    Here you can select a Pre-existing OCA Bundle
+	  </Typography>
+	</div>
+      )
+    },
+    {
+      target: '#upload-oca-bundle-button',
+      content: (
+	<div>
+	  <Typography variant="h3">Uploading a new OCA Bundle?</Typography>
+	  <Typography variant="h5">
+	    If you would like to upload your own OCA Bundle click here
+	  </Typography>
+	</div>
+      ),
+    },
+  ],
+  brandingSteps : [
+    {
+      target: '#overlay-bundle-language-select',
+      disableBeacon: true,
+      content: (
+	<div>
+	  <Typography variant="h3">Previewing Languages</Typography>
+	  <Typography variant="h5">
+	    To preview alternative languages select one of the following radio buttons
+	  </Typography>
+	</div>
+      ),
+    },
+    {
+      target: '#overlay-bundle-credential-card',
+      content: (
+	<div>
+	  <Typography variant="h3">Credential List Layout</Typography>
+	  <Typography variant="h5">
+	    This is the current Credential List Layout
+	  </Typography>
+	</div>
+      ),
+    },
+    {
+      target: '#overlay-bundle-credential-details',
+      content: (
+	<div>
+	  <Typography variant="h3">Single Credential Layout</Typography>
+	  <Typography variant="h5">
+	    Here is the current Single Credential Layout
+	  </Typography>
+	</div>
+      ),
+    },
+    {
+      target: '#overlay-bundle-branding-form',
+      content: (
+	<div>
+	  <Typography variant="h3">Customizing Branding</Typography>
+	  <Typography variant="h5">
+	    Here you can customize the branding for your overlay
+	  </Typography>
+	</div>
+      ),
+    },
+    {
+      target: "#overlay-branding-download-branding-overlay",
+      content: (
+	<div>
+	  <Typography variant="h3">Download Your Newly Created Branding</Typography>
+	  <Typography variant="h5">
+	    Here you can customize the branding for your overlay
+	  </Typography>
+	</div>
+      ),
+  }]
 }
 
 export type DemoState = "NotRunning" | "RunningIntro" | "RunningBranding" | "RunningAll"
