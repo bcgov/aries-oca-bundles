bd061152ed6555b3431774f714dfb6efb9fc2e4b
Emiliano Suñé <emiliano.sune@gmail.com>
2026-04-22 19:54:29 +0000

    chore: update docs, update devcontainer
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- scripts/buildDevcontainer.sh
+++ scripts/buildDevcontainer.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y clang
+sudo apt-get install -y clang lld
 
-git clone https://github.com/THCLab/oca-parser-xls.git
-cd ./oca-parser-xls && cargo build
-# Should check if ~/bin already exists
-mkdir ~/bin
+git clone https://github.com/bcgov/oca-parser-xls.git
+cd ./oca-parser-xls && RUSTFLAGS="-C link-arg=-fuse-ld=lld" cargo build
+mkdir -p ~/bin
 cp ./target/debug/parser ~/bin
-cd .. && rm -rf ./oca-parser-xls
+cd .. && sudo rm -rf ./oca-parser-xls
+
+# Ensure ~/bin is on PATH for future terminal sessions
+if ! grep -q 'export PATH=.*\$HOME/bin' ~/.bashrc 2>/dev/null; then
+    echo 'export PATH="$PATH:$HOME/bin"' >> ~/.bashrc
+fi
 
 exit 0
